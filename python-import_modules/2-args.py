e2bca22e05779b21a95d5819de64f4f5a0b59345
rukshar06 <saibrukshar@gmail.com>
2026-08-12 10:44:14 +0400

    task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    count = len(args)
+
+    if count == 1:
+        label = "argument"
+    else:
+        label = "arguments"
+    if count == 0:
+        punctuation = "."
+    else:
+        punctuation = ":"
+        print(str(count) + " " + label + punctuation)
+
+        position = 1
+        for arg in args:
+            print(str(position) + ":" + arg)
+            position = position + 1
