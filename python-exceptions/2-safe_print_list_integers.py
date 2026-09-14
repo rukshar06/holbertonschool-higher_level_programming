1195b4e4e10b834a675b887db80c8c28a3abbac5
rukshar06 <saibrukshar@gmail.com>
2026-09-14 11:58:19 +0400

    task 2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    if my_list is None:
+        my_list = []
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
+        except IndexError:
+            break
+    print ()
+    return count
