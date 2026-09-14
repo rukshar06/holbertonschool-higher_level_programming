71ed22515bdae9c9dc21e49eef43dc84df946862
rukshar06 <saibrukshar@gmail.com>
2026-09-14 08:44:34 +0400

    task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
     element_printed = 0
 
     try:
-        for i in range(0,x):
-            print(my_list[i], end ="")
+        for i in range(0, x):
+            print(my_list[i], end="")
             element_printed += 1
     except IndexError:
         pass
