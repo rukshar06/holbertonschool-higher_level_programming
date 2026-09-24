90c3515c8c819728f54af328259cebaebac13272
rukshar06 <saibrukshar@gmail.com>
2026-09-24 14:25:10 +0400

    task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that write a class that defines a square"""
+
+
+class Square:
+    """This function defines a square"""
+
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.size = size
