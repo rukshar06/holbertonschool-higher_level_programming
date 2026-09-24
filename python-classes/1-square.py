59b8fab293438c3107e3712a395da03a58405ffb
rukshar06 <saibrukshar@gmail.com>
2026-09-24 13:46:32 +0400

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+"""Module that define a class square"""
+
+
+class Square:
+    """this function defines a square"""
+
+    def __init__(self, size):
+
+        self.__size = size
