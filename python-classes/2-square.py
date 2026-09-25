08f336f77c80762419d1af3b5618daebf5c8aaae
rukshar06 <saibrukshar@gmail.com>
2026-09-25 09:07:49 +0400

    task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,7 +5,6 @@
 class Square:
     """This function defines a square"""
 
-
     def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError("size must be an integer")
